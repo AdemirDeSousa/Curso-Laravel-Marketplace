fa6383a2e4c1555806efa2393ba2eb8b5ddd0d74
AdemirDeSousa <jadsqj@gmail.com>
2020-06-18 18:33:24 -0300

    Mapeamento 1:N Store-Products

--- app/Store.php
+++ app/Store.php
@@ -9,5 +9,12 @@ class Store extends Model
     public function user(){
         
         return $this->belongsTo(User::class);
+        
+    }
+
+    public function products(){
+
+        return $this->hasMany(Product::class);
+
     }
 }
