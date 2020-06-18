afb842f302347d2d152669f907fc257c7c89b75e
AdemirDeSousa <jadsqj@gmail.com>
2020-06-18 19:44:26 -0300

    Mapeando N:N Products-Cetegories

--- app/Product.php
+++ app/Product.php
@@ -9,4 +9,8 @@ class Product extends Model
     public function store(){
         return $this->belongsTo(Store::class);
     }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
 }
